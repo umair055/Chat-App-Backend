50623f0a4061bc2d50a4292199fc149e1952c5f5
umair055 <umairtariq403@gmail.com>
2023-11-12 13:50:18 +0500

    URI changed

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 
 const io = socket(server, {
-  cors: { origin: "http://localhost:3000", credentials: true },
+  cors: { origin: "https://chit-chat-smoky.vercel.app", credentials: true },
 });
 
 global.onlineUsers = new Map();
